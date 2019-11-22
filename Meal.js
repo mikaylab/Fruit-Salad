93844a3edf997ce30903bb60c278cc25db0fe5dd
mikaylab <mbuford@wisc.edu>
2019-11-22 16:09:11 -0600

    Still having issues with forEach loop in Meal.js

--- Meal.js
+++ Meal.js
@@ -13,12 +13,19 @@ if (
     UIManager.setLayoutAnimationEnabledExperimental(true);
   }
 export default class Meal extends React.Component {
-    state = {expanded: false, foods: []}
-    totalAmount(list, item) {
-        if (_.isEmpty(list)) {
+    constructor(props) {
+        super(props);
+        this.state = {
+            expanded: false,
+            foods: []
+        }
+    }
+    totalAmount(item) {
+        if (_.isEmpty(this.state.foods)) {
             return 0;
         }
         let value = "";
+        const list = this.state.foods;
         switch (item) {
             case "calories": value = "calories"; break;
             case "carbohydrates": value = "carbohydrates"; break;
@@ -27,8 +34,8 @@ export default class Meal extends React.Component {
             default: console.log(`item not recognized: ${item}`); break;
         }
         let total = 0.0;
-        let array = new Array();
-        array.forEach(e => {
+
+        list.map(e => {
             total += e[value];
         });
         return total;
@@ -45,16 +52,16 @@ export default class Meal extends React.Component {
                     title={this.props.name}
                     titleStyle={styles.nameStyle}
                     rightTitle={moment(this.props.date).format("h:mm a")}
-                    subtitle={this.state.expanded ? 
+                    subtitle={this.state.expanded ?
                         <View>
-                            <Text style={styles.dataStyle}>Total Calories: {pluralize('calorie', this.totalAmount(this.state.foods, "calories"), true)}</Text>
-                            <Text style={styles.dataStyle}>Total Carbohydrates: {pluralize('unit', this.totalAmount(this.state.foods, "carbohydrates" ), true)}</Text>
-                            <Text style={styles.dataStyle}>Total Protein: {pluralize('unit', this.totalAmount(this.state.foods, "protein" ), true)}</Text>
-                            <Text style={styles.dataStyle}>Total Fat: {pluralize('gram', this.totalAmount(this.state.foods, "fat"), true)}</Text>
+                            <Text style={styles.dataStyle}>Total Calories: {pluralize('calorie', this.totalAmount("calories"), true)}</Text>
+                            <Text style={styles.dataStyle}>Total Carbohydrates: {pluralize('unit', this.totalAmount("carbohydrates"), true)}</Text>
+                            <Text style={styles.dataStyle}>Total Protein: {pluralize('unit', this.totalAmount("protein"), true)}</Text>
+                            <Text style={styles.dataStyle}>Total Fat: {pluralize('gram', this.totalAmount("fat"), true)}</Text>
                             <Text>Foods:</Text>
                             <Divider style={{backgroundColor: 'black'}}/>
                             <FlatList
-                                data={this.state.foods.reverse()}
+                                data={this.state.foods.reverse() || []}
                                 renderItem={({item}) => 
                                 {
                                     <Food id={item.id} name={item.name} carbohydrates={item.carbohydrates} fat={item.fat} protein={item.protein} calories={item.calories}/>
