e33e613de5554e57cd302dfc80a8eb420374835f
mikaylab <mbuford@wisc.edu>
2019-11-25 23:00:28 -0600

    I have gotten all the total stats for each day in GoalProgress!

--- GoalProgress.js
+++ GoalProgress.js
@@ -7,6 +7,7 @@ import getMeals from './API/meals/getMeals';
 import getMealFoods from './API/meals/foods/getMealFoods';
 import getActivity from './API/activity/getActivity';
 import sevenDayFilter from './sevenDayFilter';
+import getUserProfile from './API/user/getUserInfo';
 import moment from 'moment';
 
 export default class GoalProgress extends React.Component {
@@ -25,6 +26,7 @@ export default class GoalProgress extends React.Component {
     }
     totalAmount(list, item) {
         let value = "";
+        const _list = Array.from(list);
         switch (item) {
             case "activity": value = "duration"; break;
             case "calories": value = "calories"; break;
@@ -34,7 +36,7 @@ export default class GoalProgress extends React.Component {
             default: console.log(`item not recognized: ${item}`); break;
         }
         let total = 0.0;
-        list.forEach(e => {
+       _list.map(e => {
             total += e[value];
         });
         return Math.round(total);
@@ -50,16 +52,16 @@ export default class GoalProgress extends React.Component {
             console.log(e);
         }
     }
-    async getAllFoods() {
+    async getAllFoods(meals) {
         let foods = [];
         let _food = new Array();
-        for (const meal of this.state.meals) {
+        for (const meal of meals) {
             _food = await this.getFoodList(meal.id);
             _food.forEach((e) => {
                 foods.push(e);
             });
         }
-        this.setState({foods: foods});
+        return foods;
     }
     async getMealList() {
         try {
@@ -85,7 +87,7 @@ export default class GoalProgress extends React.Component {
             console.log(e);
         }
     }
-    organizeData() {
+    async organizeData() {
         let days = [];
         for (let i=0; i < 7; i++) {
             let day = {"name": moment().subtract(i, 'days').format("ddd"), "date": moment().subtract(i, 'days'), "meals": [], "activities": []};
@@ -93,51 +95,58 @@ export default class GoalProgress extends React.Component {
             day.meals = this.state.meals.filter(e => {
                 return moment(e.date).isSame(day.date, 'day');
             });
-            days.activities = this.state.activities.filter(e => {
+
+            day.foods = await this.getAllFoods(day.meals);
+            day.totalCalories = this.totalAmount(day.foods, "calories");
+            day.totalCarbohydrates = this.totalAmount(day.foods, "carbohydrates");
+            day.totalProtein = this.totalAmount(day.foods, "protein");
+            day.totalFat = this.totalAmount(day.foods, "fat");
+
+            day.activities = this.state.activities.filter(e => {
                 return moment(e.date).isSame(day.date, 'day');
             })
+            day.totalActivity = this.totalAmount(day.activities, "activity")
         }
-        console.log(days);
+        this.setState({days: days});
+    }
+    async getDailyGoals() {
+        let data;
+        try {
+            data = await AsyncStorage.multiGet(['@CurrentUsername', '@CurrentToken'])
+            if (data !== null) {
+                response = await getUserProfile(data[1][1], data[0][1]);
+                if (response !== null) {
+                    this.setState({dailyActivityGoal: response.goalDailyActivity|| 0.0});
+                    this.setState({dailyCaloriesGoal : response.goalDailyCalories || 0.0});
+                    this.setState({dailyCarbohydratesGoal: response.goalDailyCarbohydrates || 0.0});
+                    this.setState({dailyFatGoal: response.goalDailyFat || 0.0});
+                    this.setState({dailyProteinGoal: response.goalDailyProtein || 0.0});
+                }
+            }
+        } catch(e) {
+            console.log("Error occured getting user data");
+        }
+    }
+   async setUpPage() {
+        await this.getMealList();
+        await this.getActivityList();
+        await this.getDailyGoals();
+        await this.organizeData();
     }
     componentDidMount() {
-        this.getMealList();
-        this.getActivityList();
+        this.setUpPage();
         this.focusListener = this.props.navigation.addListener('didFocus', () => {
-            this.getMealList();
-            this.getActivityList();
+            this.setUpPage();
         });
     }
     componentWillUnmount() {
         this.focusListener.remove();
     }
     render() {
-        const data = [
-            {
-                value: 50,
-                label: 'One',
-            },
-            {
-                value: 10,
-                label: 'Two',
-            },
-            {
-                value: 40,
-                label: 'Three',
-            },
-            {
-                value: 95,
-                label: 'Four',
-            },
-            {
-                value: 85,
-                label: 'Five',
-            },
-        ]
-        this.organizeData();
         return(
             <ScrollView>
                 <Card title="Calories">
-                <ProgressChart data={data}/>
+                {/* <ProgressChart data={data}/> */}
                 </Card>
             </ScrollView>
         )
